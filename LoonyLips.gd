9120b49ff427db329d8b3f1f5736a30e262a3bb0
James Poore <jamie1224@gmail.com>
2018-07-08 17:06:37 -0400

    Section 2, Lecture 9

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,5 +4,5 @@ extends Node2D
 func _ready():
 	var prompt = ["amazing", "dragon", "Bhindi", "feared", "orc"]
 	var story = "There once was an %s %s slayer named %s.  He was so %s that even the %ss would run from him."
-	print (story % prompt)
+	$Blackboard/StoryText.bbcode_text = (story % prompt)
 	
