98c413a63bc85614fddf564968db704d7ba63bc8
James Poore <jamie1224@gmail.com>
2018-07-08 17:39:16 -0400

    Section 2, Lecture 10
    LineEdit - Text From The Player

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -4,5 +4,13 @@ extends Node2D
 func _ready():
 	var prompt = ["amazing", "dragon", "Bhindi", "feared", "orc"]
 	var story = "There once was an %s %s slayer named %s.  He was so %s that even the %ss would run from him."
-	$Blackboard/StoryText.bbcode_text = (story % prompt)
+	$Blackboard/StoryText.text = (story % prompt)
 	
+
+func _on_TextureButton_pressed():
+	var new_text = $Blackboard/TextBox.get_text()
+	_on_TextBox_text_entered(new_text)
+
+func _on_TextBox_text_entered(new_text):
+	$Blackboard/StoryText.text = new_text
+	$Blackboard/TextBox.text = ""
