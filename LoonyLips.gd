912559f0bd7e3fb9a28435e784752262908be81c
James Poore <jamie1224@gmail.com>
2018-07-08 16:47:36 -0400

    Section 2 Lecture 8

---
+++ LoonyLips.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+func _ready():
+	var prompt = ["amazing", "dragon", "Bhindi", "feared", "orc"]
+	var story = "There once was an %s %s slayer named %s.  He was so %s that even the %ss would run from him."
+	print (story % prompt)
+	
